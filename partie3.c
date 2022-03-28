eaac2702a7a3a6ae7bc3c0be44cb9c9625907e9f
Ammar434 <raoulammar434@gmail.com>
2022-03-28 19:43:06 +0200

    Avec partie 3

--- partie3.c
+++ partie3.c
@@ -1,4 +1,5 @@
 #include "partie3.h"
+
 CellKey *create_cell_key(Key *key)
 {
     CellKey *cell = malloc(sizeof(CellKey));
@@ -7,15 +8,12 @@ CellKey *create_cell_key(Key *key)
     return cell;
 }
 
-void add_key_to_head(CellKey *cellKey, Key *key)
+CellKey *add_key_to_head(CellKey *cellKey, Key *key)
 {
-    if (cellKey == NULL)
-    {
-        cellKey = create_cell_key(key);
-    }
     CellKey *newCell = create_cell_key(key);
-    newCell->next = cellKey;
+    (newCell->next) = cellKey;
     cellKey = newCell;
+    return cellKey;
 }
 
 CellKey *read_public_keys(char *filename)
@@ -27,27 +25,30 @@ CellKey *read_public_keys(char *filename)
     if (f == NULL)
     {
         printf("erreur lors de la lecture\n");
-        return;
+        return NULL;
     }
     while (fgets(buff, BUFFER_SIZE, f) != 0)
     {
-        if (sscanf(buff, "%s", pKey) != 1)
+        if (sscanf(buff, "%s ", pKey) != 1)
         {
             printf("erreur lecture\n");
             return NULL;
         }
         Key *publicKey = malloc(sizeof(Key));
         publicKey = str_to_key(pKey);
-        add_key_to_head(cell, publicKey);
+        cell = add_key_to_head(cell, publicKey);
     }
+
     fclose(f);
     return cell;
 }
 
 void print_list_keys(CellKey *LCK)
 {
+    if (!LCK)
+        return;
     CellKey *cell = LCK;
-    while (cell != NULL)
+    while (cell->next != NULL)
     {
         printf("%s\n", key_to_str(cell->key));
         cell = cell->next;
@@ -56,7 +57,7 @@ void print_list_keys(CellKey *LCK)
 
 void delete_cell_key(CellKey *c)
 {
-    free(c->data);
+    free(c->key);
     free(c);
 }
 
@@ -68,5 +69,79 @@ void delete_list_keys(CellKey *c)
         CellKey *tmp = cell;
         cell = cell->next;
         delete_cell_key(tmp);
+    }
+}
+
+CellProtected *create_cell_protected(Protected *pr)
+{
+    CellProtected *cell = (CellProtected *)malloc(sizeof(CellProtected));
+    cell->data = pr;
+    cell->next = NULL;
+    return cell;
+}
+
+CellProtected *add_cell_prototected_to_head(CellProtected *cellProtected, Protected *pr)
+{
+    CellProtected *new = create_cell_protected(pr);
+    new->next = cellProtected;
+    cellProtected = new;
+    return cellProtected;
+}
+
+CellProtected *read_protected_from_file(char *filename)
+{
+    CellProtected *cell = NULL;
+    char buff[BUFFER_SIZE];
+    char protected_text[BUFFER_SIZE];
+    FILE *f = fopen(filename, "r");
+    if (f == NULL)
+    {
+        printf("erreur lors de la lecture\n");
+        return NULL;
+    }
+    while (fgets(buff, BUFFER_SIZE, f) != 0)
+    {
+        if (sscanf(buff, "%[^\n]", protected_text) != 1)
+        {
+            printf("erreur lecture\n");
+            return NULL;
+        }
+        Protected *protected = malloc(sizeof(Protected));
+        if (!protected)
+            return NULL;
+    protected
+        = str_to_protected(protected_text);
+        cell = add_cell_prototected_to_head(cell, protected);
+    }
+    fclose(f);
+    return cell;
+}
+
+void print_list_protected(CellProtected *cellProtected)
+{
+    if (!cellProtected)
+        return;
+    CellProtected *cell = cellProtected;
+    while (cell->next != NULL)
+    {
+        printf("%s\n", protected_to_str(cell->data));
+        cell = cell->next;
+    }
+}
+
+void delete_cell_protect(CellProtected *cp)
+{
+    free(cp->data);
+    free(cp);
+}
+
+void delete_list_protected(CellProtected *c)
+{
+    CellProtected *cell = c;
+    while (cell != NULL)
+    {
+        CellProtected *tmp = cell;
+        cell = cell->next;
+        delete_cell_protect(tmp);
     }
 }
--- partie3.h
+++ partie3.h
@@ -1,10 +1,29 @@
 #ifndef PARTIE3_H
 #define PARTIE3_H
+#include "partie2.h"
 typedef struct cellKey
 {
-    Key *data;
+    Key *key;
     struct cellKey *next;
 } CellKey;
+
+typedef struct cellProtected
+{
+    Protected *data;
+    struct cellProtected *next;
+} CellProtected;
+
 CellKey *create_cell_key(Key *key);
+CellKey *add_key_to_head(CellKey *cellKey, Key *key);
+
+CellKey *read_public_keys(char *filename);
+void print_list_keys(CellKey *LCK);
+void delete_cell_key(CellKey *c);
+void delete_list_keys(CellKey *c);
+CellProtected *create_cell_protected(Protected *pr);
+CellProtected *read_protected_from_file(char *filename);
+void print_list_protected(CellProtected *cellProtected);
+void delete_cell_protect(CellProtected *cp);
+void delete_list_protected(CellProtected *c);
 
 #endif // PARTIE3_H
